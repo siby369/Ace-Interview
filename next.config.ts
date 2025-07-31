14fb739968021663816ee7f26d158dc0d274e80b
siby369 <sibyraj369@gmail.com>
2025-08-01 10:00:00 +2130

    added gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ require('dotenv').config();
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
