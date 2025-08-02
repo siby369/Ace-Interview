013a3a9a606fe177596ce8fe27a53a6c771fb5de
siby369 <sibyraj369@gmail.com>
2025-08-02 09:54:56 +0530

    Some changes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -27,22 +27,6 @@ const CardHeader = React.forwardRef<
     {...props}
   />
 ))
-CardHeader.displayName = "CardHeader"
-
-const CardTitle = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className
-    )}
-    {...props}
-  />
-))
-CardTitle.displayName = "CardTitle"
 
 const CardDescription = React.forwardRef<
   HTMLDivElement,
