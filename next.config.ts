6c49ab33261c00776388c6ef46348a675d5ecc82
siby369 <sibyraj369@gmail.com>
2025-10-22 14:07:00 +0530

    Add Next.js configuration file to set TypeScript and ESLint options, and configure remote image patterns for external sources.

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+require('dotenv').config();
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
