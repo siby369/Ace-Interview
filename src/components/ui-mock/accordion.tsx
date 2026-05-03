b3193de310a0c68a07d9bcbe8dffc979ca76e628
siby369 <sibyraj369@gmail.com>
2026-05-03 08:25:57 +0000

    feat(ui): export Accordion component and add displayName

--- src/components/ui-mock/accordion.tsx
+++ src/components/ui-mock/accordion.tsx
@@ -6,3 +6,7 @@ export interface AccordionProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Accordion = React.forwardRef<HTMLDivElement, AccordionProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Accordion.displayName = "Accordion";
+
+export { Accordion };
