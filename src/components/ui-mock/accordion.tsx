39aa01bc0edefeae5ce469b55c8731e2d59db270
siby369 <sibyraj369@gmail.com>
2026-05-03 10:25:30 +0000

    feat(ui): implement base rendering for Accordion

--- src/components/ui-mock/accordion.tsx
+++ src/components/ui-mock/accordion.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface AccordionProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Accordion = React.forwardRef<HTMLDivElement, AccordionProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
