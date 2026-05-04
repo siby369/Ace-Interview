64955e1f8bca6e10a8eb97e620d73addf0e0bff9
siby369 <sibyraj369@gmail.com>
2026-05-04 05:30:35 +0000

    feat(ui): export Alert component and add displayName

--- src/components/ui-mock/alert.tsx
+++ src/components/ui-mock/alert.tsx
@@ -6,3 +6,7 @@ export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Alert.displayName = "Alert";
+
+export { Alert };
