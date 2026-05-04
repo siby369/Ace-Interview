9fcd9ed35b5af8a007190fd93c4cbdf5ea219865
siby369 <sibyraj369@gmail.com>
2026-05-04 04:09:18 +0000

    feat(ui): implement base rendering for Alert

--- src/components/ui-mock/alert.tsx
+++ src/components/ui-mock/alert.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
