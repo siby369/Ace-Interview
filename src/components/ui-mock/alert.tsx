bbc34a8a54ee2d135beadc40a14e9b366acb1273
siby369 <sibyraj369@gmail.com>
2026-05-04 03:59:23 +0000

    style(ui): add tailwind classes to Alert

--- src/components/ui-mock/alert.tsx
+++ src/components/ui-mock/alert.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
