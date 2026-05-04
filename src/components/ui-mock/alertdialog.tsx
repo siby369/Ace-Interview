05d0b8e2d957849a307979cc16edac9dc1c0432f
siby369 <sibyraj369@gmail.com>
2026-05-04 04:01:16 +0000

    feat(ui): implement base rendering for AlertDialog

--- src/components/ui-mock/alertdialog.tsx
+++ src/components/ui-mock/alertdialog.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface AlertDialogProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const AlertDialog = React.forwardRef<HTMLDivElement, AlertDialogProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
