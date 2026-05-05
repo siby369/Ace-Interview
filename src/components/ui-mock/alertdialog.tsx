ebd78bf1a7630efc81885335c7eb34ceba7ad92a
siby369 <sibyraj369@gmail.com>
2026-05-05 07:54:09 +0000

    style(ui): add tailwind classes to AlertDialog

--- src/components/ui-mock/alertdialog.tsx
+++ src/components/ui-mock/alertdialog.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface AlertDialogProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const AlertDialog = React.forwardRef<HTMLDivElement, AlertDialogProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
