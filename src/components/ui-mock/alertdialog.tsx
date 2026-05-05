e0863a1935e29241af95220e8152dd39ba16d320
siby369 <sibyraj369@gmail.com>
2026-05-05 07:29:16 +0000

    feat(ui): export AlertDialog component and add displayName

--- src/components/ui-mock/alertdialog.tsx
+++ src/components/ui-mock/alertdialog.tsx
@@ -6,3 +6,7 @@ export interface AlertDialogProps extends React.HTMLAttributes<HTMLDivElement> {
 const AlertDialog = React.forwardRef<HTMLDivElement, AlertDialogProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+AlertDialog.displayName = "AlertDialog";
+
+export { AlertDialog };
