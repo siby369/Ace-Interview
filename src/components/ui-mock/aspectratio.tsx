b96e624602f3af2b1e0efa2fc00ad0c1a95afea0
siby369 <sibyraj369@gmail.com>
2026-05-06 07:53:19 +0000

    style(ui): add tailwind classes to AspectRatio

--- src/components/ui-mock/aspectratio.tsx
+++ src/components/ui-mock/aspectratio.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface AspectRatioProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const AspectRatio = React.forwardRef<HTMLDivElement, AspectRatioProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
