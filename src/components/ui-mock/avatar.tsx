19daf93ad7624db5c147aeedde14e9062ca30ec8
siby369 <sibyraj369@gmail.com>
2026-05-06 10:56:03 +0000

    feat(ui): implement base rendering for Avatar

--- src/components/ui-mock/avatar.tsx
+++ src/components/ui-mock/avatar.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface AvatarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
