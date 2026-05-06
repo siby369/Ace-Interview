d003a8291bb9bb271901c78f8635ab75a1d4890d
siby369 <sibyraj369@gmail.com>
2026-05-06 07:58:29 +0000

    feat(ui): export AspectRatio component and add displayName

--- src/components/ui-mock/aspectratio.tsx
+++ src/components/ui-mock/aspectratio.tsx
@@ -6,3 +6,7 @@ export interface AspectRatioProps extends React.HTMLAttributes<HTMLDivElement> {
 const AspectRatio = React.forwardRef<HTMLDivElement, AspectRatioProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+AspectRatio.displayName = "AspectRatio";
+
+export { AspectRatio };
