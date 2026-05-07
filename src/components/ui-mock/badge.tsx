fdc29c6df358496fc7f1fe1d73ccfca493bd4ee7
siby369 <sibyraj369@gmail.com>
2026-05-07 04:01:45 +0000

    feat(ui): implement base rendering for Badge

--- src/components/ui-mock/badge.tsx
+++ src/components/ui-mock/badge.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
