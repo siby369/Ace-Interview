71e3574b55dd02c32d3b436c56fd14a41238b088
siby369 <sibyraj369@gmail.com>
2026-05-08 09:57:07 +0000

    style(ui): add tailwind classes to Badge

--- src/components/ui-mock/badge.tsx
+++ src/components/ui-mock/badge.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
