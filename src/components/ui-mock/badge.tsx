36b9c03a47940803de98a9e21baa9f237899a39d
siby369 <sibyraj369@gmail.com>
2026-05-08 06:59:11 +0000

    feat(ui): export Badge component and add displayName

--- src/components/ui-mock/badge.tsx
+++ src/components/ui-mock/badge.tsx
@@ -6,3 +6,7 @@ export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Badge.displayName = "Badge";
+
+export { Badge };
