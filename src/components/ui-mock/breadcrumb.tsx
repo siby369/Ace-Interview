0f3a2a0ef15c01e73f46e5f4f4699e205aed46e4
siby369 <sibyraj369@gmail.com>
2026-05-08 11:06:09 +0000

    style(ui): add tailwind classes to Breadcrumb

--- src/components/ui-mock/breadcrumb.tsx
+++ src/components/ui-mock/breadcrumb.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface BreadcrumbProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Breadcrumb = React.forwardRef<HTMLDivElement, BreadcrumbProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
