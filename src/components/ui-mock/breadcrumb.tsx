f0d43c9b9ca9607164d3160b7990adaa136856d3
siby369 <sibyraj369@gmail.com>
2026-05-09 07:28:07 +0000

    feat(ui): export Breadcrumb component and add displayName

--- src/components/ui-mock/breadcrumb.tsx
+++ src/components/ui-mock/breadcrumb.tsx
@@ -6,3 +6,7 @@ export interface BreadcrumbProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Breadcrumb = React.forwardRef<HTMLDivElement, BreadcrumbProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Breadcrumb.displayName = "Breadcrumb";
+
+export { Breadcrumb };
