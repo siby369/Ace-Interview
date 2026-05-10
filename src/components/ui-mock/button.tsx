6b7b322842f90ef992ebc84c3c85dcc7677dbf9b
siby369 <sibyraj369@gmail.com>
2026-05-10 09:35:17 +0000

    feat(ui): export Button component and add displayName

--- src/components/ui-mock/button.tsx
+++ src/components/ui-mock/button.tsx
@@ -6,3 +6,7 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Button = React.forwardRef<HTMLDivElement, ButtonProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Button.displayName = "Button";
+
+export { Button };
