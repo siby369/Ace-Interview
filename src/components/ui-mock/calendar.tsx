ff3a49cb4cf9bf839dc733840db676b4c3f68892
siby369 <sibyraj369@gmail.com>
2026-05-11 08:37:13 +0000

    feat(ui): implement base rendering for Calendar

--- src/components/ui-mock/calendar.tsx
+++ src/components/ui-mock/calendar.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface CalendarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Calendar = React.forwardRef<HTMLDivElement, CalendarProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
