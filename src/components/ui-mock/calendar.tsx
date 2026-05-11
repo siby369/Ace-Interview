a1995bfecf92f4ea07c25a488d4cc5e9565f67bb
siby369 <sibyraj369@gmail.com>
2026-05-11 09:23:22 +0000

    style(ui): add tailwind classes to Calendar

--- src/components/ui-mock/calendar.tsx
+++ src/components/ui-mock/calendar.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface CalendarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Calendar = React.forwardRef<HTMLDivElement, CalendarProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
