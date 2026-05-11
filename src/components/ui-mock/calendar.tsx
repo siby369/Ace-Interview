e5f271554978e411a5e7c86b2c154ea3d8eacf6d
siby369 <sibyraj369@gmail.com>
2026-05-11 06:06:33 +0000

    feat(ui): export Calendar component and add displayName

--- src/components/ui-mock/calendar.tsx
+++ src/components/ui-mock/calendar.tsx
@@ -6,3 +6,7 @@ export interface CalendarProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Calendar = React.forwardRef<HTMLDivElement, CalendarProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Calendar.displayName = "Calendar";
+
+export { Calendar };
