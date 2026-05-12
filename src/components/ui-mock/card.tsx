b9a22ff4cd45fdab21b26ea0c3049a28df1a28aa
siby369 <sibyraj369@gmail.com>
2026-05-12 05:53:22 +0000

    feat(ui): implement base rendering for Card

--- src/components/ui-mock/card.tsx
+++ src/components/ui-mock/card.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Card = React.forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
