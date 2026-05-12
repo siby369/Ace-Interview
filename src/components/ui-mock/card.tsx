58182f492827ec79f7842b01c6a90114357a3fa6
siby369 <sibyraj369@gmail.com>
2026-05-12 07:05:38 +0000

    style(ui): add tailwind classes to Card

--- src/components/ui-mock/card.tsx
+++ src/components/ui-mock/card.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Card = React.forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
