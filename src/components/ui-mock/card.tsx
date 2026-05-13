2b972565eda09c8a3cdab197bde244b71d665fb9
siby369 <sibyraj369@gmail.com>
2026-05-13 10:52:02 +0000

    feat(ui): export Card component and add displayName

--- src/components/ui-mock/card.tsx
+++ src/components/ui-mock/card.tsx
@@ -6,3 +6,7 @@ export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Card = React.forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Card.displayName = "Card";
+
+export { Card };
