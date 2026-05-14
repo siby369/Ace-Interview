ca10ffa91d0a7ef1a3dd8464d3d47ca716e7731a
siby369 <sibyraj369@gmail.com>
2026-05-14 11:18:29 +0000

    feat(ui): export Carousel component and add displayName

--- src/components/ui-mock/carousel.tsx
+++ src/components/ui-mock/carousel.tsx
@@ -6,3 +6,7 @@ export interface CarouselProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Carousel.displayName = "Carousel";
+
+export { Carousel };
