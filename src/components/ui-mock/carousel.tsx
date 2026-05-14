1b1c592ae2d322ce2cf2e61296f486597f384e9d
siby369 <sibyraj369@gmail.com>
2026-05-14 04:42:17 +0000

    style(ui): add tailwind classes to Carousel

--- src/components/ui-mock/carousel.tsx
+++ src/components/ui-mock/carousel.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface CarouselProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
