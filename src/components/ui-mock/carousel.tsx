40da0466957a0f62d483b2eafe108863b2b435ee
siby369 <sibyraj369@gmail.com>
2026-05-14 10:58:07 +0000

    feat(ui): implement base rendering for Carousel

--- src/components/ui-mock/carousel.tsx
+++ src/components/ui-mock/carousel.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface CarouselProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
