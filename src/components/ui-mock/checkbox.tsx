4395509b13a9705edf3f2e25495dea8df33fab1c
siby369 <sibyraj369@gmail.com>
2026-05-15 05:42:06 +0000

    feat(ui): implement base rendering for Checkbox

--- src/components/ui-mock/checkbox.tsx
+++ src/components/ui-mock/checkbox.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface CheckboxProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Checkbox = React.forwardRef<HTMLDivElement, CheckboxProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
