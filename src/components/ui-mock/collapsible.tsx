c2678577045d0c22ce99d7a88f44a198ffc78bc4
siby369 <sibyraj369@gmail.com>
2026-05-15 10:57:09 +0000

    feat(ui): implement base rendering for Collapsible

--- src/components/ui-mock/collapsible.tsx
+++ src/components/ui-mock/collapsible.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface CollapsibleProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Collapsible = React.forwardRef<HTMLDivElement, CollapsibleProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
