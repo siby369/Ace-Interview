4ffa05130dbf059e7047a12878b2e2de91c83675
siby369 <sibyraj369@gmail.com>
2026-05-15 08:40:20 +0000

    feat(ui): export Checkbox component and add displayName

--- src/components/ui-mock/checkbox.tsx
+++ src/components/ui-mock/checkbox.tsx
@@ -6,3 +6,7 @@ export interface CheckboxProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Checkbox = React.forwardRef<HTMLDivElement, CheckboxProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Checkbox.displayName = "Checkbox";
+
+export { Checkbox };
