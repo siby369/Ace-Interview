af947d321a66dc62994ada61c4dcee9d352f90bd
siby369 <sibyraj369@gmail.com>
2026-05-16 04:15:55 +0000

    feat(ui): implement base rendering for Command

--- src/components/ui-mock/command.tsx
+++ src/components/ui-mock/command.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface CommandProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Command = React.forwardRef<HTMLDivElement, CommandProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
