a285aa9f240942b37d74ffca5ffbaf80bd4d1267
siby369 <sibyraj369@gmail.com>
2026-05-16 03:48:01 +0000

    style(ui): add tailwind classes to Collapsible

--- src/components/ui-mock/collapsible.tsx
+++ src/components/ui-mock/collapsible.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface CollapsibleProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Collapsible = React.forwardRef<HTMLDivElement, CollapsibleProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
