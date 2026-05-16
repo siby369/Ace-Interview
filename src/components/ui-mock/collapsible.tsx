d004722900d533db0b4331f9bb355a2921eb0fb3
siby369 <sibyraj369@gmail.com>
2026-05-16 07:54:14 +0000

    feat(ui): export Collapsible component and add displayName

--- src/components/ui-mock/collapsible.tsx
+++ src/components/ui-mock/collapsible.tsx
@@ -6,3 +6,7 @@ export interface CollapsibleProps extends React.HTMLAttributes<HTMLDivElement> {
 const Collapsible = React.forwardRef<HTMLDivElement, CollapsibleProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Collapsible.displayName = "Collapsible";
+
+export { Collapsible };
