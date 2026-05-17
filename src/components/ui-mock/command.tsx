74e70bf9c134d44cff622f57863742f9c1d17686
siby369 <sibyraj369@gmail.com>
2026-05-17 06:16:09 +0000

    feat(ui): export Command component and add displayName

--- src/components/ui-mock/command.tsx
+++ src/components/ui-mock/command.tsx
@@ -6,3 +6,7 @@ export interface CommandProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Command = React.forwardRef<HTMLDivElement, CommandProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Command.displayName = "Command";
+
+export { Command };
