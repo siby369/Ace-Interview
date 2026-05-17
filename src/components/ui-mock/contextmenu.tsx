db626b3cb711d0564d89656dc4e021067189b4db
siby369 <sibyraj369@gmail.com>
2026-05-17 06:58:46 +0000

    feat(ui): implement base rendering for ContextMenu

--- src/components/ui-mock/contextmenu.tsx
+++ src/components/ui-mock/contextmenu.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ContextMenuProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const ContextMenu = React.forwardRef<HTMLDivElement, ContextMenuProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
