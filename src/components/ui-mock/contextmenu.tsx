78c85eb7716d387937cf7549fe935ae629c33407
siby369 <sibyraj369@gmail.com>
2026-05-17 07:57:57 +0000

    style(ui): add tailwind classes to ContextMenu

--- src/components/ui-mock/contextmenu.tsx
+++ src/components/ui-mock/contextmenu.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface ContextMenuProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const ContextMenu = React.forwardRef<HTMLDivElement, ContextMenuProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
