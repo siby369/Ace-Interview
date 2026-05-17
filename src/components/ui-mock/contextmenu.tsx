0b33771f77b6f78a4d19df54a4ebd31596f701ce
siby369 <sibyraj369@gmail.com>
2026-05-17 04:28:09 +0000

    feat(ui): export ContextMenu component and add displayName

--- src/components/ui-mock/contextmenu.tsx
+++ src/components/ui-mock/contextmenu.tsx
@@ -6,3 +6,7 @@ export interface ContextMenuProps extends React.HTMLAttributes<HTMLDivElement> {
 const ContextMenu = React.forwardRef<HTMLDivElement, ContextMenuProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+ContextMenu.displayName = "ContextMenu";
+
+export { ContextMenu };
