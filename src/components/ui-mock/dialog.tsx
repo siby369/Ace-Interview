b0e88f022be5ec7af83fcc66f52b47ffee59cc9f
siby369 <sibyraj369@gmail.com>
2026-05-18 07:01:57 +0000

    feat(ui): export Dialog component and add displayName

--- src/components/ui-mock/dialog.tsx
+++ src/components/ui-mock/dialog.tsx
@@ -6,3 +6,7 @@ export interface DialogProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Dialog = React.forwardRef<HTMLDivElement, DialogProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Dialog.displayName = "Dialog";
+
+export { Dialog };
