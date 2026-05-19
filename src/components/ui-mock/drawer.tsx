8e4e371f34e0db1780b6bdbb746f13900c7fcb45
siby369 <sibyraj369@gmail.com>
2026-05-19 10:03:20 +0000

    style(ui): add tailwind classes to Drawer

--- src/components/ui-mock/drawer.tsx
+++ src/components/ui-mock/drawer.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface DrawerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Drawer = React.forwardRef<HTMLDivElement, DrawerProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
