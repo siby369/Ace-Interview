aa819e160a4f0ce460f9582ef00224c82107659e
siby369 <sibyraj369@gmail.com>
2026-05-19 08:53:32 +0000

    feat(ui): export Drawer component and add displayName

--- src/components/ui-mock/drawer.tsx
+++ src/components/ui-mock/drawer.tsx
@@ -6,3 +6,7 @@ export interface DrawerProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Drawer = React.forwardRef<HTMLDivElement, DrawerProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Drawer.displayName = "Drawer";
+
+export { Drawer };
