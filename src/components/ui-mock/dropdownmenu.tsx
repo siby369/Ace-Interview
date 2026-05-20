032388b4408804ddf59dfbf07c0d6d101a3175b9
siby369 <sibyraj369@gmail.com>
2026-05-20 04:02:19 +0000

    feat(ui): implement base rendering for DropdownMenu

--- src/components/ui-mock/dropdownmenu.tsx
+++ src/components/ui-mock/dropdownmenu.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface DropdownMenuProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const DropdownMenu = React.forwardRef<HTMLDivElement, DropdownMenuProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
