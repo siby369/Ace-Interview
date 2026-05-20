93b552d4c73699ea805363c48a7c6d6e83636e9d
siby369 <sibyraj369@gmail.com>
2026-05-20 08:49:37 +0000

    feat(ui): export DropdownMenu component and add displayName

--- src/components/ui-mock/dropdownmenu.tsx
+++ src/components/ui-mock/dropdownmenu.tsx
@@ -6,3 +6,7 @@ export interface DropdownMenuProps extends React.HTMLAttributes<HTMLDivElement> 
 const DropdownMenu = React.forwardRef<HTMLDivElement, DropdownMenuProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+DropdownMenu.displayName = "DropdownMenu";
+
+export { DropdownMenu };
