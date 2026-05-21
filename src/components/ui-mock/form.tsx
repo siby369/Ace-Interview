7ab2003620de85f2569b6643c84bb74d24ecb299
siby369 <sibyraj369@gmail.com>
2026-05-21 05:23:27 +0000

    feat(ui): export Form component and add displayName

--- src/components/ui-mock/form.tsx
+++ src/components/ui-mock/form.tsx
@@ -6,3 +6,7 @@ export interface FormProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Form = React.forwardRef<HTMLDivElement, FormProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Form.displayName = "Form";
+
+export { Form };
