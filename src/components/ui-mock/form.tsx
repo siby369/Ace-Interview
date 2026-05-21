bbd15ace458cd39aa0a375f2d7c63d83048760cc
siby369 <sibyraj369@gmail.com>
2026-05-21 05:52:15 +0000

    style(ui): add tailwind classes to Form

--- src/components/ui-mock/form.tsx
+++ src/components/ui-mock/form.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface FormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Form = React.forwardRef<HTMLDivElement, FormProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
