2991883780b88a344dec5ace566e0e74bb22d828
siby369 <sibyraj369@gmail.com>
2026-05-21 09:59:07 +0000

    feat(ui): implement base rendering for Form

--- src/components/ui-mock/form.tsx
+++ src/components/ui-mock/form.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface FormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Form = React.forwardRef<HTMLDivElement, FormProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
