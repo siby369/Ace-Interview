e7fa7af34bd810b4c517f6cdd23de74b41edcdaf
siby369 <sibyraj369@gmail.com>
2026-05-22 10:08:32 +0000

    style(ui): add tailwind classes to HoverCard

--- src/components/ui-mock/hovercard.tsx
+++ src/components/ui-mock/hovercard.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface HoverCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const HoverCard = React.forwardRef<HTMLDivElement, HoverCardProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
