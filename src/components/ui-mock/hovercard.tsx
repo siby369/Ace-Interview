279975b79e27f5a7b9dda68911dc8680d98622ce
siby369 <sibyraj369@gmail.com>
2026-05-22 04:31:46 +0000

    feat(ui): export HoverCard component and add displayName

--- src/components/ui-mock/hovercard.tsx
+++ src/components/ui-mock/hovercard.tsx
@@ -6,3 +6,7 @@ export interface HoverCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 const HoverCard = React.forwardRef<HTMLDivElement, HoverCardProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+HoverCard.displayName = "HoverCard";
+
+export { HoverCard };
