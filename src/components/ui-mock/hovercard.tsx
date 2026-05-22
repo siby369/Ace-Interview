50371e20b4f9a114200dc2fde4ce2c9cdd599592
siby369 <sibyraj369@gmail.com>
2026-05-22 10:28:26 +0000

    feat(ui): implement base rendering for HoverCard

--- src/components/ui-mock/hovercard.tsx
+++ src/components/ui-mock/hovercard.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface HoverCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const HoverCard = React.forwardRef<HTMLDivElement, HoverCardProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
