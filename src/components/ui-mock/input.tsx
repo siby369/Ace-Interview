0739452f46ef5c8703e965700ab87ad9a375cd52
siby369 <sibyraj369@gmail.com>
2026-05-23 05:03:50 +0000

    feat(ui): export Input component and add displayName

--- src/components/ui-mock/input.tsx
+++ src/components/ui-mock/input.tsx
@@ -6,3 +6,7 @@ export interface InputProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Input = React.forwardRef<HTMLDivElement, InputProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Input.displayName = "Input";
+
+export { Input };
