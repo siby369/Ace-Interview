c90d689f2955a41566187badb25885816c49508a
siby369 <sibyraj369@gmail.com>
2026-05-23 07:27:47 +0000

    style(ui): add tailwind classes to Input

--- src/components/ui-mock/input.tsx
+++ src/components/ui-mock/input.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface InputProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Input = React.forwardRef<HTMLDivElement, InputProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
