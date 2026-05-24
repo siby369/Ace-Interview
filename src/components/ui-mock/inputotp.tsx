01c7d14a05fd094ec9710b6333c7b9b9f8f4675c
siby369 <sibyraj369@gmail.com>
2026-05-24 06:32:25 +0000

    feat(ui): implement base rendering for InputOTP

--- src/components/ui-mock/inputotp.tsx
+++ src/components/ui-mock/inputotp.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface InputOTPProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const InputOTP = React.forwardRef<HTMLDivElement, InputOTPProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
