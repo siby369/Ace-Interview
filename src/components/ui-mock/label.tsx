4757c67979134102a5f272fd3a71407eaf0c0d7b
siby369 <sibyraj369@gmail.com>
2026-05-25 11:15:18 +0000

    style(ui): add tailwind classes to Label

--- src/components/ui-mock/label.tsx
+++ src/components/ui-mock/label.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface LabelProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Label = React.forwardRef<HTMLDivElement, LabelProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
