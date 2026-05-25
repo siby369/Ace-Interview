108f68c9aac027d0dc2409c0ae08f87c7d148250
siby369 <sibyraj369@gmail.com>
2026-05-25 05:01:00 +0000

    feat(ui): implement base rendering for Label

--- src/components/ui-mock/label.tsx
+++ src/components/ui-mock/label.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface LabelProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Label = React.forwardRef<HTMLDivElement, LabelProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
