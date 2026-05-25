0173c69b6d2f21954a07f2c030528d90de0db2bb
siby369 <sibyraj369@gmail.com>
2026-05-25 08:30:24 +0000

    feat(ui): export Label component and add displayName

--- src/components/ui-mock/label.tsx
+++ src/components/ui-mock/label.tsx
@@ -6,3 +6,7 @@ export interface LabelProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Label = React.forwardRef<HTMLDivElement, LabelProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Label.displayName = "Label";
+
+export { Label };
