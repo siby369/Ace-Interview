d74306abc6dfdd2b4611a1f71aa12a8a7ae6bb43
siby369 <sibyraj369@gmail.com>
2026-05-26 03:34:07 +0000

    feat(ui): implement base rendering for Menubar

--- src/components/ui-mock/menubar.tsx
+++ src/components/ui-mock/menubar.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface MenubarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Menubar = React.forwardRef<HTMLDivElement, MenubarProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
