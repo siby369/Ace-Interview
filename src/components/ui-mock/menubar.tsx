bc3292e17703a894ae89da8ece7c150f6459563e
siby369 <sibyraj369@gmail.com>
2026-05-26 11:07:21 +0000

    feat(ui): export Menubar component and add displayName

--- src/components/ui-mock/menubar.tsx
+++ src/components/ui-mock/menubar.tsx
@@ -6,3 +6,7 @@ export interface MenubarProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Menubar = React.forwardRef<HTMLDivElement, MenubarProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Menubar.displayName = "Menubar";
+
+export { Menubar };
