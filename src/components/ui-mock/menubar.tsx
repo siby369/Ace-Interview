239e6a72799c48dc2bc335b73a96bb22e2b4cbc5
siby369 <sibyraj369@gmail.com>
2026-05-26 04:20:12 +0000

    style(ui): add tailwind classes to Menubar

--- src/components/ui-mock/menubar.tsx
+++ src/components/ui-mock/menubar.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface MenubarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Menubar = React.forwardRef<HTMLDivElement, MenubarProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
