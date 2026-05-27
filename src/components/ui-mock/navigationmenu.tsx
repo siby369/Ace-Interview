062c70bcf094a95490b926bd0b2777034dc3f264
siby369 <sibyraj369@gmail.com>
2026-05-27 04:19:22 +0000

    feat(ui): export NavigationMenu component and add displayName

--- src/components/ui-mock/navigationmenu.tsx
+++ src/components/ui-mock/navigationmenu.tsx
@@ -6,3 +6,7 @@ export interface NavigationMenuProps extends React.HTMLAttributes<HTMLDivElement
 const NavigationMenu = React.forwardRef<HTMLDivElement, NavigationMenuProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+NavigationMenu.displayName = "NavigationMenu";
+
+export { NavigationMenu };
