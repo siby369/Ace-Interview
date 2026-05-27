519e0a6ddcde5f828b2b3fc636f9835facbfc26a
siby369 <sibyraj369@gmail.com>
2026-05-27 04:51:16 +0000

    style(ui): add tailwind classes to NavigationMenu

--- src/components/ui-mock/navigationmenu.tsx
+++ src/components/ui-mock/navigationmenu.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface NavigationMenuProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const NavigationMenu = React.forwardRef<HTMLDivElement, NavigationMenuProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
