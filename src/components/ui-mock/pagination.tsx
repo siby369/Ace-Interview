bdc97aee0bc84d2e736d35b198e25ec407fcb7a3
siby369 <sibyraj369@gmail.com>
2026-05-28 06:49:27 +0000

    feat(ui): implement base rendering for Pagination

--- src/components/ui-mock/pagination.tsx
+++ src/components/ui-mock/pagination.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface PaginationProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
