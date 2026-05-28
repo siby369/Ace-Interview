a9ee2766ac339b14618563f940340875d7b078dd
siby369 <sibyraj369@gmail.com>
2026-05-28 08:30:30 +0000

    style(ui): add tailwind classes to Pagination

--- src/components/ui-mock/pagination.tsx
+++ src/components/ui-mock/pagination.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface PaginationProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
