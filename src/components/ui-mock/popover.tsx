183d87759626939bbc8b73c3dfd680575636855e
siby369 <sibyraj369@gmail.com>
2026-05-29 06:47:05 +0000

    feat(ui): export Popover component and add displayName

--- src/components/ui-mock/popover.tsx
+++ src/components/ui-mock/popover.tsx
@@ -6,3 +6,7 @@ export interface PopoverProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Popover = React.forwardRef<HTMLDivElement, PopoverProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Popover.displayName = "Popover";
+
+export { Popover };
