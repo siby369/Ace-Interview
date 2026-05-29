89440ee5cc070eb789636dfdcc0e0dde58165977
siby369 <sibyraj369@gmail.com>
2026-05-29 07:56:55 +0000

    style(ui): add tailwind classes to Popover

--- src/components/ui-mock/popover.tsx
+++ src/components/ui-mock/popover.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface PopoverProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Popover = React.forwardRef<HTMLDivElement, PopoverProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
