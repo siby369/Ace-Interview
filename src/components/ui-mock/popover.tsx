a15d6e6ec23f44a0116e270a7aab8e54651c0c17
siby369 <sibyraj369@gmail.com>
2026-05-29 04:48:47 +0000

    feat(ui): implement base rendering for Popover

--- src/components/ui-mock/popover.tsx
+++ src/components/ui-mock/popover.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface PopoverProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Popover = React.forwardRef<HTMLDivElement, PopoverProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
