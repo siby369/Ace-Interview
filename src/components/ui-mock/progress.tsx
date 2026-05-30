ae823c5c2650cc14917dc892c924a514f146cdce
siby369 <sibyraj369@gmail.com>
2026-05-30 08:29:35 +0000

    feat(ui): implement base rendering for Progress

--- src/components/ui-mock/progress.tsx
+++ src/components/ui-mock/progress.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Progress = React.forwardRef<HTMLDivElement, ProgressProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
