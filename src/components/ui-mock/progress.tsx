e98ae8cdb56540e3d6e208fc9fa7983a0cb21e54
siby369 <sibyraj369@gmail.com>
2026-05-30 07:08:54 +0000

    feat(ui): export Progress component and add displayName

--- src/components/ui-mock/progress.tsx
+++ src/components/ui-mock/progress.tsx
@@ -6,3 +6,7 @@ export interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Progress = React.forwardRef<HTMLDivElement, ProgressProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Progress.displayName = "Progress";
+
+export { Progress };
