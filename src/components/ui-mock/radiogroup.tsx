104707751cf85b00a171217d4eac2c3981a0c654
siby369 <sibyraj369@gmail.com>
2026-05-31 08:22:29 +0000

    feat(ui): implement base rendering for RadioGroup

--- src/components/ui-mock/radiogroup.tsx
+++ src/components/ui-mock/radiogroup.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface RadioGroupProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
