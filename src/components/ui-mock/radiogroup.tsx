586dfda4a1cf22dac58ad4bdf68d85745eb3009f
siby369 <sibyraj369@gmail.com>
2026-05-31 04:09:43 +0000

    feat(ui): export RadioGroup component and add displayName

--- src/components/ui-mock/radiogroup.tsx
+++ src/components/ui-mock/radiogroup.tsx
@@ -6,3 +6,7 @@ export interface RadioGroupProps extends React.HTMLAttributes<HTMLDivElement> {}
 const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+RadioGroup.displayName = "RadioGroup";
+
+export { RadioGroup };
