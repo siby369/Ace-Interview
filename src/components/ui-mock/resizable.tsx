8c5ef65651a7dd1e8cd71b5af06ffaaf06332b27
siby369 <sibyraj369@gmail.com>
2026-06-01 04:38:34 +0000

    style(ui): add tailwind classes to Resizable

--- src/components/ui-mock/resizable.tsx
+++ src/components/ui-mock/resizable.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface ResizableProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Resizable = React.forwardRef<HTMLDivElement, ResizableProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
