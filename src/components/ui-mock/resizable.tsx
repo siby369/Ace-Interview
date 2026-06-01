02b4c4006adf2352253b94b2901618f2253642d3
siby369 <sibyraj369@gmail.com>
2026-06-01 07:12:21 +0000

    feat(ui): implement base rendering for Resizable

--- src/components/ui-mock/resizable.tsx
+++ src/components/ui-mock/resizable.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ResizableProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Resizable = React.forwardRef<HTMLDivElement, ResizableProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
