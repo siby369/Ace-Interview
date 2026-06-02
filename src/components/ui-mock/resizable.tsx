956b09c28a73fb7c108c6681238283477205150c
siby369 <sibyraj369@gmail.com>
2026-06-02 05:17:04 +0000

    feat(ui): export Resizable component and add displayName

--- src/components/ui-mock/resizable.tsx
+++ src/components/ui-mock/resizable.tsx
@@ -6,3 +6,7 @@ export interface ResizableProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Resizable = React.forwardRef<HTMLDivElement, ResizableProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Resizable.displayName = "Resizable";
+
+export { Resizable };
