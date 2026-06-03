c00d038c84975978442e461a1d411025610c42fc
siby369 <sibyraj369@gmail.com>
2026-06-03 06:36:05 +0000

    feat(ui): export Select component and add displayName

--- src/components/ui-mock/select.tsx
+++ src/components/ui-mock/select.tsx
@@ -6,3 +6,7 @@ export interface SelectProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Select = React.forwardRef<HTMLDivElement, SelectProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Select.displayName = "Select";
+
+export { Select };
