a76c3321c6f5d2dfb9d8f0bd6adbe33e70ffd6c3
siby369 <sibyraj369@gmail.com>
2026-06-03 10:43:51 +0000

    style(ui): add tailwind classes to Select

--- src/components/ui-mock/select.tsx
+++ src/components/ui-mock/select.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface SelectProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Select = React.forwardRef<HTMLDivElement, SelectProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
