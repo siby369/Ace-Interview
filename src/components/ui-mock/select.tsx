c10e46304a585cd0ef931bab153a192ebdce47df
siby369 <sibyraj369@gmail.com>
2026-06-03 09:59:44 +0000

    feat(ui): implement base rendering for Select

--- src/components/ui-mock/select.tsx
+++ src/components/ui-mock/select.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface SelectProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Select = React.forwardRef<HTMLDivElement, SelectProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
