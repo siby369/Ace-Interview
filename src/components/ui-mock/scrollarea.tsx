1bcd312c0a239b2420e830395388336eaa1d6bda
siby369 <sibyraj369@gmail.com>
2026-06-03 09:20:03 +0000

    feat(ui): export ScrollArea component and add displayName

--- src/components/ui-mock/scrollarea.tsx
+++ src/components/ui-mock/scrollarea.tsx
@@ -6,3 +6,7 @@ export interface ScrollAreaProps extends React.HTMLAttributes<HTMLDivElement> {}
 const ScrollArea = React.forwardRef<HTMLDivElement, ScrollAreaProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+ScrollArea.displayName = "ScrollArea";
+
+export { ScrollArea };
