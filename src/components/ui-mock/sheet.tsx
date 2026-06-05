fb6a3b905b95f4ab1a386adcf7157f8596063857
siby369 <sibyraj369@gmail.com>
2026-06-05 05:48:40 +0000

    feat(ui): implement base rendering for Sheet

--- src/components/ui-mock/sheet.tsx
+++ src/components/ui-mock/sheet.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface SheetProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Sheet = React.forwardRef<HTMLDivElement, SheetProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
