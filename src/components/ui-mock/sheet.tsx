362959c194aafd906b3930d9a53d8e0185445c9e
siby369 <sibyraj369@gmail.com>
2026-06-05 04:33:58 +0000

    style(ui): add tailwind classes to Sheet

--- src/components/ui-mock/sheet.tsx
+++ src/components/ui-mock/sheet.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface SheetProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Sheet = React.forwardRef<HTMLDivElement, SheetProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
