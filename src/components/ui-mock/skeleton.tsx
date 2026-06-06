3bf3580e2ad3b8d3871135c2fcad8792f3df4dac
siby369 <sibyraj369@gmail.com>
2026-06-06 08:09:09 +0000

    feat(ui): export Skeleton component and add displayName

--- src/components/ui-mock/skeleton.tsx
+++ src/components/ui-mock/skeleton.tsx
@@ -6,3 +6,7 @@ export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Skeleton.displayName = "Skeleton";
+
+export { Skeleton };
