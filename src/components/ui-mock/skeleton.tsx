77bf8826134dd8c052db64e3eca371b74c857f02
siby369 <sibyraj369@gmail.com>
2026-06-06 08:21:44 +0000

    feat(ui): implement base rendering for Skeleton

--- src/components/ui-mock/skeleton.tsx
+++ src/components/ui-mock/skeleton.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
