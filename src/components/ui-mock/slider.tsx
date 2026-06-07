e2258dfcac3c867fe0a79df440a87619af40d841
siby369 <sibyraj369@gmail.com>
2026-06-07 08:06:43 +0000

    feat(ui): export Slider component and add displayName

--- src/components/ui-mock/slider.tsx
+++ src/components/ui-mock/slider.tsx
@@ -6,3 +6,7 @@ export interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Slider = React.forwardRef<HTMLDivElement, SliderProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Slider.displayName = "Slider";
+
+export { Slider };
