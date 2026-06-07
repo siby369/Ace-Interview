f23bcdb048ebc4c46436b5ac030f9e3f560edeaf
siby369 <sibyraj369@gmail.com>
2026-06-07 11:09:22 +0000

    feat(ui): implement base rendering for Slider

--- src/components/ui-mock/slider.tsx
+++ src/components/ui-mock/slider.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Slider = React.forwardRef<HTMLDivElement, SliderProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
