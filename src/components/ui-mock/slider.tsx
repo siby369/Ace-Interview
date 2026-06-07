ab60105b6d845535c68f669eb93472fd3c47247b
siby369 <sibyraj369@gmail.com>
2026-06-07 07:13:32 +0000

    style(ui): add tailwind classes to Slider

--- src/components/ui-mock/slider.tsx
+++ src/components/ui-mock/slider.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Slider = React.forwardRef<HTMLDivElement, SliderProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
