e87b0ade95543d7afe63cc3dbb8230abca947d20
siby369 <sibyraj369@gmail.com>
2026-06-08 07:44:34 +0000

    feat(ui): implement base rendering for Sonner

--- src/components/ui-mock/sonner.tsx
+++ src/components/ui-mock/sonner.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface SonnerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Sonner = React.forwardRef<HTMLDivElement, SonnerProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
