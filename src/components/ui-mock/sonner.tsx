5108596e0853d0c8be04b98f478bfc61e614ce12
siby369 <sibyraj369@gmail.com>
2026-06-08 09:59:50 +0000

    feat(ui): export Sonner component and add displayName

--- src/components/ui-mock/sonner.tsx
+++ src/components/ui-mock/sonner.tsx
@@ -6,3 +6,7 @@ export interface SonnerProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Sonner = React.forwardRef<HTMLDivElement, SonnerProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Sonner.displayName = "Sonner";
+
+export { Sonner };
