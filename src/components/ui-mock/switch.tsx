df7cc24fb54c96bd0fce5bf79bd63b56ae4935ad
siby369 <sibyraj369@gmail.com>
2026-06-09 04:25:32 +0000

    feat(ui): implement base rendering for Switch

--- src/components/ui-mock/switch.tsx
+++ src/components/ui-mock/switch.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface SwitchProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Switch = React.forwardRef<HTMLDivElement, SwitchProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
