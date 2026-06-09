113c56c1b4a939a17584425f65d6be4290d09e06
siby369 <sibyraj369@gmail.com>
2026-06-09 10:40:47 +0000

    style(ui): add tailwind classes to Switch

--- src/components/ui-mock/switch.tsx
+++ src/components/ui-mock/switch.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface SwitchProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Switch = React.forwardRef<HTMLDivElement, SwitchProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
