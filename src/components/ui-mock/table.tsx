a4fa6554260c63e459350248baa813d0a789a410
siby369 <sibyraj369@gmail.com>
2026-06-10 04:57:49 +0000

    feat(ui): implement base rendering for Table

--- src/components/ui-mock/table.tsx
+++ src/components/ui-mock/table.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface TableProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Table = React.forwardRef<HTMLDivElement, TableProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
