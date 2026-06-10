e023e099ba31d69fcbd2bf95bc330410d7006a18
siby369 <sibyraj369@gmail.com>
2026-06-10 04:50:02 +0000

    feat(ui): export Switch component and add displayName

--- src/components/ui-mock/switch.tsx
+++ src/components/ui-mock/switch.tsx
@@ -6,3 +6,7 @@ export interface SwitchProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Switch = React.forwardRef<HTMLDivElement, SwitchProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Switch.displayName = "Switch";
+
+export { Switch };
