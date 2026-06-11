49f62832901534a5d3f8119016bcac267499b519
siby369 <sibyraj369@gmail.com>
2026-06-11 09:23:03 +0000

    style(ui): add tailwind classes to Table

--- src/components/ui-mock/table.tsx
+++ src/components/ui-mock/table.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface TableProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Table = React.forwardRef<HTMLDivElement, TableProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
