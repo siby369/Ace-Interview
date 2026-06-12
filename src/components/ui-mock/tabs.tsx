e957bfe0187076fd4b0cfbaaf653aadb2f356a70
siby369 <sibyraj369@gmail.com>
2026-06-12 05:34:14 +0000

    style(ui): add tailwind classes to Tabs

--- src/components/ui-mock/tabs.tsx
+++ src/components/ui-mock/tabs.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Tabs = React.forwardRef<HTMLDivElement, TabsProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
