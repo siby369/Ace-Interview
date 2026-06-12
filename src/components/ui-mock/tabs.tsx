d71ff29fab9a231d552050bcc1f7f77380139245
siby369 <sibyraj369@gmail.com>
2026-06-12 09:05:28 +0000

    feat(ui): export Tabs component and add displayName

--- src/components/ui-mock/tabs.tsx
+++ src/components/ui-mock/tabs.tsx
@@ -6,3 +6,7 @@ export interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Tabs = React.forwardRef<HTMLDivElement, TabsProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Tabs.displayName = "Tabs";
+
+export { Tabs };
