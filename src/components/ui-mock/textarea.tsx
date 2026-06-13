7db5fcafcc3871269ded3a7be3e383c5464e1f1f
siby369 <sibyraj369@gmail.com>
2026-06-13 09:11:21 +0000

    feat(ui): export Textarea component and add displayName

--- src/components/ui-mock/textarea.tsx
+++ src/components/ui-mock/textarea.tsx
@@ -6,3 +6,7 @@ export interface TextareaProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Textarea = React.forwardRef<HTMLDivElement, TextareaProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Textarea.displayName = "Textarea";
+
+export { Textarea };
