e3dd71eed03366c36b59c8937e7075ce6764b8dd
siby369 <sibyraj369@gmail.com>
2026-06-13 09:53:09 +0000

    feat(ui): implement base rendering for Textarea

--- src/components/ui-mock/textarea.tsx
+++ src/components/ui-mock/textarea.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface TextareaProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Textarea = React.forwardRef<HTMLDivElement, TextareaProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
