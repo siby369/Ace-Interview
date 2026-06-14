099f22ed4045fb2ce67ae095c3b351a0793ae4b7
siby369 <sibyraj369@gmail.com>
2026-06-14 05:14:38 +0000

    style(ui): add tailwind classes to Toast

--- src/components/ui-mock/toast.tsx
+++ src/components/ui-mock/toast.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Toast = React.forwardRef<HTMLDivElement, ToastProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
