7790c9167869e2d5ff3bc767dc4314b2ba9540b8
siby369 <sibyraj369@gmail.com>
2026-06-14 05:26:41 +0000

    feat(ui): export Toast component and add displayName

--- src/components/ui-mock/toast.tsx
+++ src/components/ui-mock/toast.tsx
@@ -6,3 +6,7 @@ export interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Toast = React.forwardRef<HTMLDivElement, ToastProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Toast.displayName = "Toast";
+
+export { Toast };
