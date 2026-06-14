68d8d526ae289103b7aeb8d9a1f2f44a3d8746ee
siby369 <sibyraj369@gmail.com>
2026-06-14 10:07:20 +0000

    feat(ui): implement base rendering for Toast

--- src/components/ui-mock/toast.tsx
+++ src/components/ui-mock/toast.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Toast = React.forwardRef<HTMLDivElement, ToastProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
