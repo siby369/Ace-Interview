4ec10f83ddae5939972aacb218fc78a4ff712dcc
siby369 <sibyraj369@gmail.com>
2026-06-15 09:01:30 +0000

    feat(ui): implement base rendering for Toggle

--- src/components/ui-mock/toggle.tsx
+++ src/components/ui-mock/toggle.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ToggleProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Toggle = React.forwardRef<HTMLDivElement, ToggleProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
