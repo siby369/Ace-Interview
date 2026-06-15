4b63f5ab5df4169ed5b5f266ad547835264bd842
siby369 <sibyraj369@gmail.com>
2026-06-15 09:44:59 +0000

    feat(ui): export Toggle component and add displayName

--- src/components/ui-mock/toggle.tsx
+++ src/components/ui-mock/toggle.tsx
@@ -6,3 +6,7 @@ export interface ToggleProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Toggle = React.forwardRef<HTMLDivElement, ToggleProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Toggle.displayName = "Toggle";
+
+export { Toggle };
