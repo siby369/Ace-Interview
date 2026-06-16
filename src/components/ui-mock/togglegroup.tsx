34ae469442c3f92049d9a7a38517cb8d4b6b3d67
siby369 <sibyraj369@gmail.com>
2026-06-16 10:05:36 +0000

    feat(ui): implement base rendering for ToggleGroup

--- src/components/ui-mock/togglegroup.tsx
+++ src/components/ui-mock/togglegroup.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ToggleGroupProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const ToggleGroup = React.forwardRef<HTMLDivElement, ToggleGroupProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
