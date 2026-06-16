d318ca11ad9a5eeee5d98eb0e2276b147a29204e
siby369 <sibyraj369@gmail.com>
2026-06-16 05:14:53 +0000

    feat(ui): export ToggleGroup component and add displayName

--- src/components/ui-mock/togglegroup.tsx
+++ src/components/ui-mock/togglegroup.tsx
@@ -6,3 +6,7 @@ export interface ToggleGroupProps extends React.HTMLAttributes<HTMLDivElement> {
 const ToggleGroup = React.forwardRef<HTMLDivElement, ToggleGroupProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+ToggleGroup.displayName = "ToggleGroup";
+
+export { ToggleGroup };
