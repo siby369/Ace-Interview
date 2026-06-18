d3c1b5fe870f51261d9e457c27a3a78d45853dd2
siby369 <sibyraj369@gmail.com>
2026-06-18 04:04:46 +0000

    feat(ui): implement base rendering for Chart

--- src/components/ui-mock/chart.tsx
+++ src/components/ui-mock/chart.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface ChartProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const Chart = React.forwardRef<HTMLDivElement, ChartProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
