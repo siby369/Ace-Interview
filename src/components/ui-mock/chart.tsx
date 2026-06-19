b3dfbcf3342eac217b90112ae957662b9c62e08e
siby369 <sibyraj369@gmail.com>
2026-06-19 09:53:13 +0000

    feat(ui): export Chart component and add displayName

--- src/components/ui-mock/chart.tsx
+++ src/components/ui-mock/chart.tsx
@@ -6,3 +6,7 @@ export interface ChartProps extends React.HTMLAttributes<HTMLDivElement> {}
 const Chart = React.forwardRef<HTMLDivElement, ChartProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+Chart.displayName = "Chart";
+
+export { Chart };
