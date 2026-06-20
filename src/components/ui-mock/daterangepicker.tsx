b0da58147177e86aff8a9933525973c007335349
siby369 <sibyraj369@gmail.com>
2026-06-20 10:45:21 +0000

    feat(ui): export DateRangePicker component and add displayName

--- src/components/ui-mock/daterangepicker.tsx
+++ src/components/ui-mock/daterangepicker.tsx
@@ -6,3 +6,7 @@ export interface DateRangePickerProps extends React.HTMLAttributes<HTMLDivElemen
 const DateRangePicker = React.forwardRef<HTMLDivElement, DateRangePickerProps>(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
+
+DateRangePicker.displayName = "DateRangePicker";
+
+export { DateRangePicker };
