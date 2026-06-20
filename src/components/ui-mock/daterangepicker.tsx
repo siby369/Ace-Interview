0ae22880cc91fc2decca06364d4fc2e67d912301
siby369 <sibyraj369@gmail.com>
2026-06-20 10:31:08 +0000

    feat(ui): implement base rendering for DateRangePicker

--- src/components/ui-mock/daterangepicker.tsx
+++ src/components/ui-mock/daterangepicker.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface DateRangePickerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const DateRangePicker = React.forwardRef<HTMLDivElement, DateRangePickerProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
