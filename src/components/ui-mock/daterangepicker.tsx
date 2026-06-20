34d00451d8712c0b14c4b999e2cbd9e07dd36a96
siby369 <sibyraj369@gmail.com>
2026-06-20 08:58:13 +0000

    style(ui): add tailwind classes to DateRangePicker

--- src/components/ui-mock/daterangepicker.tsx
+++ src/components/ui-mock/daterangepicker.tsx
@@ -4,5 +4,5 @@ import { cn } from "@/lib/utils";
 export interface DateRangePickerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const DateRangePicker = React.forwardRef<HTMLDivElement, DateRangePickerProps>(({ className, ...props }, ref) => {
-  return <div ref={ref} className={cn("", className)} {...props} />;
+  return <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />;
 });
