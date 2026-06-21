cd4d37c92f70102a9725fabab6c6992b1182fa32
siby369 <sibyraj369@gmail.com>
2026-06-21 03:42:08 +0000

    feat(ui): implement base rendering for TimePicker

--- src/components/ui-mock/timepicker.tsx
+++ src/components/ui-mock/timepicker.tsx
@@ -3,3 +3,6 @@ import { cn } from "@/lib/utils";
 
 export interface TimePickerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const TimePicker = React.forwardRef<HTMLDivElement, TimePickerProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("", className)} {...props} />;
+});
